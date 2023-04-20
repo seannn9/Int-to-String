da5a88e4dbc23c80272a06ba3acb056bd82f406c
seannn9 <naeskirlu9@gmail.com>
2023-04-20 22:48:07 +0800

    added some code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main() {
+std::string convert(int n) {
+    
+}
 
+int main() {
+    int n;
+    do {
+        std::cout << "Enter number: ";
+        std::cin >> n; 
+    } while (n <= 0 || n > 2147483647);
+    std::cout << convert(n);
     return 0;
 }
